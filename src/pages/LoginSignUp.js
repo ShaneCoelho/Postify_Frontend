6674970092cf947bff81eb6925a671f6a8214dbc
ShaneCoelho <shanecoelho1@gmail.com>
2024-01-03 17:04:54 +0530

    created the store and downloaded the redux toolkit dependencies

--- src/pages/LoginSignUp.js
+++ src/pages/LoginSignUp.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Helmet } from 'react-helmet';
+import { useState } from "react";
 import './LoginSignUp.css';
 
 const LoginSignUp = () => {
+
+    const [signup, setSignUp] = useState({});
+
+    const getUserSignUpData = (e) => {
+        setSignUp({ ...signup, [e.target.name]: e.target.value });
+      };
+    
+
     return (
         <>
             <Helmet>
@@ -20,9 +29,9 @@ const LoginSignUp = () => {
                         <label htmlFor="chk" aria-hidden="true">
                             Sign up
                         </label>
-                        <input type="text" name="txt" placeholder="User name" required="" />
-                        <input type="email" name="email" placeholder="Email" required="" />
-                        <input type="password" name="pswd" placeholder="Password" required="" />
+                        <input type="text" name="username" placeholder="User name" required="" onChange={getUserSignUpData}/>
+                        <input type="email" name="email" placeholder="Email" required="" onChange={getUserSignUpData}/>
+                        <input type="password" name="password" placeholder="Password" required="" onChange={getUserSignUpData}/>
                         <button>Sign up</button>
                     </form>
                 </div>
